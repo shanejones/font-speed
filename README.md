519092d1b695dc839e10aa645428d03f53d888b5
Shane Jones <shane@under2.agency>
2019-11-27 15:43:04 +0000

    Initial commit with the front page

---
+++ README.md
@@ -0,0 +1,3 @@
+# Font Speed test
+
+I'll write a blog post about this when I'm done.
---
+++ source/index.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('head')
+<title>Font Speed Experiment - Shane Jones - Under2</title>
+@endsection
+
+@section('content')
+
+<main class="main p-4">
+    <h1 class="font-light text-3xl text-black">Font Speed Experiment</h1>
+    <p class="text-gray-600 mt-4">An experiment by <a href="https://twitter.com/shanejones">Shane Jones</a> from <a href="http://under2.agency/">Under2</a></p>
+</main>
+
+@endsection
