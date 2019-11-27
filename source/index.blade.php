6d58e268a328bb3ab35221ad1288ad7902dcb1a9
Shane Jones <shane@under2.agency>
2019-11-27 16:53:52 +0000

    trailing slashes

--- source/index.blade.php
+++ source/index.blade.php
@@ -14,12 +14,12 @@
         <div class="sm:w-1/2">
             <h2 class="font-light text-3xl text-black">Google Hosted</h2>
             <ul>
-                <li><a href="google-host-standard">Standard Installation</a></li>
-                <li><a href="google-host-subset">Standard Installation with specific subset</a></li>
-                <li><a href="google-host-standard-preconnect">Standard Installation with preconnect</a></li>
-                <li><a href="google-host-subset-preconnect">Standard Installation with specific subset and preconnect</a></li>
-                <li><a href="google-host-webfont-loader">Webfont Loader</a></li>
-                <li><a href="google-host-webfont-loader-subset">Webfont Loader with specific subset</a></li>
+                <li><a href="google-host-standard/">Standard Installation</a></li>
+                <li><a href="google-host-subset/">Standard Installation with specific subset</a></li>
+                <li><a href="google-host-standard-preconnect/">Standard Installation with preconnect</a></li>
+                <li><a href="google-host-subset-preconnect/">Standard Installation with specific subset and preconnect</a></li>
+                <li><a href="google-host-webfont-loader/">Webfont Loader</a></li>
+                <li><a href="google-host-webfont-loader-subset/">Webfont Loader with specific subset</a></li>
             </ul>
         </div>
         <div class="sm:w-1/2">
